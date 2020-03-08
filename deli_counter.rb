42c5397970a1d68e4e212f68be59e5fa47465d2b
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-08 21:41:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    order = "The line is currently:"
-    array.each.with_index(1) do |name, i|
-      order << "#{i}. #{name}"
+    current_line = "The line is currently:"
+    array.each.with_index(1) do |name, index|
+      current_line << "#{index}. #{name}"
     end
-    puts order
+    puts current_line
   end
 end
 
