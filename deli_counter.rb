ba51dfb077b2fcf9d10f6fae485acd04443302c3
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-08 21:39:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,4 +15,11 @@ def take_a_number (katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving (name)
+  if name.length== 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{name.first}."
+    name.shift
+  end
+end
