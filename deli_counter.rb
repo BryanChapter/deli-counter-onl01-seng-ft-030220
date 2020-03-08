074ea4fc037d77debd3c653458dbc45aef797cfb
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-08 21:38:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    order = "The line is currently:"
+    array.each.with_index(1) do |name, i|
+      order << "#{i}. #{name}"
+    end
+    puts order
+  end
+end
+
+def take_a_number (katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving
